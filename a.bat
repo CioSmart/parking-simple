bfd304767c2164c9ef6d79925acc6e1f75f4668a
CioSmart <vcioceanu@gmail.com>
2026-04-27 12:44:18 +0300

    fix supabase schema config

--- a.bat
+++ a.bat
@@ -1,8 +1,3 @@
-cd C:\Proiecte\aplicatie parcare\parking-simple
-rm -rf .git
-git init
 git add .
-git commit -m "init: fork from parking-app"
-# creezi repo pe GitHub: parking-simple
-git remote add origin https://github.com/CioSmart/parking-simple.git
-git push -u origin main
+git commit -m "fix supabase schema config"
+git push
